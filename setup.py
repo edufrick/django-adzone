af8aa8880398c662f2cfbed27a3c70032b2440b2
francisco souza <fsouza@users.noreply.github.com>
2020-07-03 20:26:21 -0400

    Bump 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="django-adzone",
-    version="0.2.4",
+    version="0.2.5",
     url="http://github.com/winterweaver/django-adzone",
     description="A django app to manage adverts according to zones on a website.",
     author="Andre Engelbrecht",
